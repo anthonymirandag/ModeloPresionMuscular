a9e051b3a664d093dcff75c19017eec367980cd0
AnthonyPC <AnthonyPC@DESKTOP-FBIAEO9.mshome.net>
2020-08-30 00:28:22 -0500

    simulaciones

--- ModeloUnicompartimental_parameters.m
+++ ModeloUnicompartimental_parameters.m
@@ -3,9 +3,11 @@ clear all
 close all
 
 %Parametros del Modelo
-R = 20; % resistencia de la vía aérea
-C = 0.015; % complianza o distensibilidad
+R = 4; % resistencia de la vía aérea
+C = 0.05; % complianza o distensibilidad
 Pvent = 20; % a presión ventilatoria
-
-K = 0.1; % contanstate Pmus = K * V/C
+% Simlucaion 1: 1 cmH2O. K = 0.1
+% Simlucaion 2: 1.7 cmH2O. K = 0.17
+% Simlucaion 3: 5 cmH2O. K = 0.5
+K = 0.5 ; % contanstate Pmus = K * V/C
 sim('ModeloUnicompartimental')
